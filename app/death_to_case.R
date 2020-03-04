1a1fe59b0a0fabd1130d48b2557607290da841bf
Amy Gimma <amyg225@gmail.com>
2020-03-04 00:40:27 +0000

    fix typo

--- app/death_to_case.R
+++ app/death_to_case.R
@@ -27,7 +27,7 @@ ui <- fluidPage(
     div(
         HTML("<p style='max-width:95%;'><strong>Authors</strong>: 
           Thibaut Jombart, Sam Abbott, Amy Gimma, Kevin Zandvoort, Sam Clifford,
-          Christopher Jarvis, Timothy Russel, Sebastian Funk, Hamrish Gibbs, 
+          Christopher Jarvis, Timothy Russell, Sebastian Funk, Hamrish Gibbs, 
           Rosalind Eggo, Adam Kucharski, 
           <a href='https://cmmid.github.io/groups/ncov-group'>
           <i>CMMID COVID-19 Working Group*</i></a>,
